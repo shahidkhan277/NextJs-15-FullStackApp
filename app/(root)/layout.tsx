69ba0a8cb3c6b3f4e8bf7f4fdb5eae66c43ab967
MUHAMMAD SHAHID KHAN <afridi.sk1720@gmail.com>
2024-12-14 18:02:34 +0500

    first-commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '@/app/components/Navbar'
+
+
+const layout = ({children}:Readonly<{children:React.ReactNode}>) => {
+  return (
+    <main className='font-work-sans'>
+        <Navbar />
+        {children}
+    </main>
+  )
+}
+
+export default layout
